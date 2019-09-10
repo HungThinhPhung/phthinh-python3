5b04823300dda791c45ffc63539720033da088fb
phthinh <nbhoa@rd.misa.com.vn>
2019-09-10 11:10:38 +0700

    update package

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup
 setup(
     name='phthinh',
-    version='0.1',
-    packages=find_packages(),
+    version='0.2',
+    packages=[
+        'phthinh',
+    ],
     url='',
     license='MIT',
     author='phthinh',
     author_email='hungthinhphung1996@gmail.com',
     description='Most Frequently Used Python3 Code of Hung Thinh Phung'
 )
+
